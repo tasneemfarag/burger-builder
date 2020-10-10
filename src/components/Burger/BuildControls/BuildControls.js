d9297f1a603c4db00889d47269df661e4c4960db
Tasneem Farag <tasneem.a.farag@gmail.com>
2020-10-09 20:09:13 -0400

    fixing an error with label rendering

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -3,17 +3,17 @@ import classes from './BuildControls.css';
 import BuildControl from './BuildControl/BuildControl';
 
 const controls = [
-    { lable: 'Salad', type: 'salad' },
-    { lable: 'Bacon', type: 'bacon' },
-    { lable: 'Cheese', type: 'cheese' },
-    { lable: 'Meet', type: 'meet' },
+    { label: 'Salad', type: 'salad' },
+    { label: 'Bacon', type: 'bacon' },
+    { label: 'Cheese', type: 'cheese' },
+    { label: 'Meet', type: 'meet' },
 
 ];
 
 const buildControls = (props) => (
    <div className={classes.BuildControls}>
        {controls.map(ctrl => (
-           <BuildControl key={ctrl.lable} lable={ctrl.lable}/>
+           <BuildControl key={ctrl.label} label={ctrl.label}/>
        ))}
    </div>
 );
