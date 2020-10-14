9ae8a477db38dbc0189011398e7ad86e8cc5aa10
Tasneem Farag <tasneem.a.farag@gmail.com>
2020-10-14 14:02:13 -0400

    improving performance by using lifecycle methods

--- src/components/Burger/OrderSummery/OrderSummery.js
+++ src/components/Burger/OrderSummery/OrderSummery.js
@@ -1,28 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Aux from '../../../hoc/Aux';
 import Button from '../../UI/Button/Button';
 
-const orderSummery = (props) => {
-    const ingredientSummery = Object.keys(props.ingredients)
+class OrderSummery extends Component {
+    componentWillUpdate() {
+        console.log('[OrderSummery] will update');
+    }
+
+
+    render () {
+
+        const ingredientSummery = Object.keys(this.props.ingredients)
         .map(igKey => {
             return (
                 <li key={igKey}>
-                    <span style={{ textTransform: 'capitalize' }}>{igKey}</span>: {props.ingredients[igKey]}
+                    <span style={{ textTransform: 'capitalize' }}>{igKey}</span>: {this.props.ingredients[igKey]}
                 </li>);
         })
-    return (
-        <Aux>
+
+        return (
+            <Aux>
             <h3>Your Order</h3>
             <p>A delicious burger with the following ingredients:</p>
             <ul>
                 {ingredientSummery}
             </ul>
-            <p><strong>Total Price: {props.price}</strong></p>
+            <p><strong>Total Price: {this.props.price}</strong></p>
             <p>Continue to checkout? </p>
-            <Button btnType="Danger" clicked={props.purchaseCancelled}>CANCEL</Button>
-            <Button btnType="Success" clicked={props.purchaseContinued}>CONTENUE</Button>
+            <Button btnType="Danger" clicked={this.props.purchaseCancelled}>CANCEL</Button>
+            <Button btnType="Success" clicked={this.props.purchaseContinued}>CONTENUE</Button>
         </Aux>
-    );
-};
+        );
+    }
+}
 
-export default orderSummery; 
+export default OrderSummery; 
