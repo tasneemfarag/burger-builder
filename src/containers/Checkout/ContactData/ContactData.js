11e62b69ce29719c98bb2705b55b9f877e837883
Tasneem Farag <tasneem.a.farag@gmail.com>
2020-10-27 16:27:36 -0400

    setting up a JS config for the form

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -8,32 +8,68 @@ import Input from '../../../components/UI/Input/Input';
 
 class ContactData extends Component {
     state = {
-        name: '',
-        email: '',
-        address: {
-            street: '',
-            postalCode: ''
+        orderForm: {
+            name: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Your Name'
+                },
+                value: ''
+            },
+            street: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Street'
+                },
+                value: ''
+            },
+            zipCode: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'ZIP Code'
+                },
+                value: ''
+            },
+            country: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Country'
+                },
+                value: ''
+            },
+            email: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'email',
+                    placeholder: 'Your E-Mail'
+                },
+                value: ''
+            },
+            deliveryMethod: {
+                elementType: 'select',
+                elementConfig: {
+                   options: [
+                       {value: 'fastest', displayValue: 'Fastest'},
+                       {value: 'cheapest', displayValue: 'Cheapest'}
+                    ]
+                },
+                value: ''
+            }
         },
         loading: false
     }
 
     orderHandler = (e) => {
         e.preventDefault();
-        
+
         this.setState({ loading: true });
         const order = {
             ingredients: this.props.ingredients,
             price: this.props.price,
-            customer: {
-                name: 'Tasneem Farag',
-                address: {
-                    street: 'Sesame Street',
-                    zipcode: '86439',
-                    country: 'Happyland'
-                },
-                email: 'test@test.com'
-            },
-            deliveryMethod: 'fastest'
         }
         axios.post('/orders.json', order)
             .then(response => {
@@ -48,18 +84,18 @@ class ContactData extends Component {
 
     render() {
         let form = (
-                <form>
-                    <Input inputtype="input" type="text" name="name" placeholder="Your Name" />
-                    <Input inputtype="input" type="email" name="email" placeholder="Your Email" />
-                    <Input inputtype="input" type="text" name="street" placeholder="Your Street" />
-                    <Input inputtype="input" type="text" name="postal" placeholder="Postal Code" />
-                    <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
-                </form>
+            <form>
+                <Input elementType="..."  elementConfig="..." value="..."/>
+                <Input inputtype="input" type="email" name="email" placeholder="Your Email" />
+                <Input inputtype="input" type="text" name="street" placeholder="Your Street" />
+                <Input inputtype="input" type="text" name="postal" placeholder="Postal Code" />
+                <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
+            </form>
         );
         if (this.state.loading) {
             form = <Spinner />;
         }
-        return(
+        return (
             <div className={classes.ContactData}>
                 <h4>Enter your Contact Data</h4>
                 {form}
