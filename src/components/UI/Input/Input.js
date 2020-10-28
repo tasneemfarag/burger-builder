44eb13b58135393df80eeb089747d661a93a4427
Tasneem Farag <tasneem.a.farag@gmail.com>
2020-10-28 11:07:11 -0400

    adding a dropdown component

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -4,20 +4,32 @@ import classes from './Input.css'
 
 const input = (props) => {
 
-    let inputElement = null; 
+    let inputElement = null;
 
     switch (props.elementType) {
-        case ('input'): 
+        case ('input'):
             inputElement = <input className={classes.InputElement}
-             {...props.elementConfig} value={props.value}/>;
+                {...props.elementConfig} value={props.value} />;
             break;
         case ('textarea'):
             inputElement = <textarea className={classes.InputElement}
-             {...props.elementConfig} value={props.value}/>;
+                {...props.elementConfig} value={props.value} />;
+            break;
+        case ('select'):
+            inputElement = (
+                <select className={classes.InputElement}
+                    value={props.value}>
+                    {props.elementConfig.options.map(option => (
+                        <option key={option.value} value={option.value}>
+                            {option.displayValue}
+                        </option>
+                    ))}
+                </select>
+            );
             break;
         default:
             inputElement = <input className={classes.InputElement}
-             {...props.elementConfig} value={props.value}/>;       
+                {...props.elementConfig} value={props.value} />;
     }
     return (
         <div className={classes.Input}>
